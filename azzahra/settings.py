d879c365910ea83a3921b6fda220dea627267fab
Hessam <ivyblackmail@gmail.com>
2016-08-26 10:10:21 +0430

    fix sending email

--- azzahra/settings.py
+++ azzahra/settings.py
@@ -117,6 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
