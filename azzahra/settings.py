cd5e078b000f01e3473de7f839ac353df9caaac6
reza <mosavi1990@gmail.com>
2016-09-07 11:11:25 +0430

    emailaddress correct

--- azzahra/settings.py
+++ azzahra/settings.py
@@ -118,11 +118,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = False
+EMAIL_HOST = 'mehr.sharif.ir'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bar.ehsan74@gmail.com'
-EMAIL_HOST_PASSWORD = 'ehsan1379'
+EMAIL_HOST_USER = 'azzahra@mehr.sharif.ir'
+EMAIL_HOST_PASSWORD = 'hei@1391'
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
