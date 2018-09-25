b8185ad3272118a9ef57fded0c365e8834aa6123
Omid Baharloo <omid.gonbad@gmail.com>
2018-09-25 11:12:51 +0330

    change status

--- manage_app/views.py
+++ manage_app/views.py
@@ -19,12 +19,12 @@ class RegistrationManagement(viewsets.GenericViewSet, mixins.RetrieveModelMixin,
 
     @decorators.action(detail=True)
     def messages(self, request, *args, **kwargs):
-        return response.Response(MessageInRegistrationSerializer(self.get_object().messages.all(),many=True).data)
+        return response.Response(MessageInRegistrationSerializer(self.get_object().messages.all(), many=True).data)
 
     @decorators.action(detail=True)
     def payments(self, request, *args, **kwargs):
         from pay.serializers import PaymentInRegistrationSerializer
-        return response.Response(PaymentInRegistrationSerializer(self.get_object().payments.all(),many=True).data)
+        return response.Response(PaymentInRegistrationSerializer(self.get_object().payments.all(), many=True).data)
 
     @decorators.action(detail=True, methods=['POST', ])
     def new_message(self, request, *args, **kwargs):
@@ -35,6 +35,18 @@ class RegistrationManagement(viewsets.GenericViewSet, mixins.RetrieveModelMixin,
             from omid_utils.sms import sendSMS
             sendSMS([message.registration.profile.mobile], message.text)
         return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @decorators.action(detail=True, methods=['POST', ])
+    def change_status(self, request, *args, **kwargs):
+        new_status = request.data.get('status', Registration.STATUS_DEFAULT)
+        registration = self.get_object()
+        registration.status = new_status
+        registration.save()
+        couple = registration.get_couple_registration()
+        if couple:
+            couple.status = new_status
+            couple.save()
+        return response.Response(RegistrationInManageSerializer(registration).data)
 
 
 class ProgramManagement(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.UpdateModelMixin):
