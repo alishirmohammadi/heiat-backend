e60ef78e426ddfd00e5a425279f14482ee801dd2
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2020-02-05 02:39:19 +0330

    update requirements.txt

--- dining/views.py
+++ dining/views.py
@@ -4,20 +4,9 @@ from rest_framework import decorators, permissions, response
 
 from accounts.models import Profile
 from dining.models import Meal
-from program.models import Program, Question
+from program.models import Program
 
 is_test = True
-paboos = Program.objects.get(title="پابوس عشق ۹۸")
-paboos_f = Program.objects.get(title="پابوس عشق فارغ التحصیلان ۹۸")
-lunches = ((
-               Question.objects.get(program=paboos, title="ناهار روز اول"),
-               Question.objects.get(program=paboos, title="ناهار روز دوم"),
-               Question.objects.get(program=paboos, title="ناهار روز سوم"),
-           ), (
-               Question.objects.get(program=paboos_f, title="ناهار روز اول"),
-               Question.objects.get(program=paboos_f, title="ناهار روز دوم"),
-               Question.objects.get(program=paboos_f, title="ناهار روز سوم"),
-           ))
 
 
 @decorators.api_view(['POST'])
