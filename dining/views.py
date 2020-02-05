30d24c1c9e1230583f4cde0fdecdc7e42c583a56
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2020-02-05 19:37:01 +0330

    response status is always 200

--- dining/views.py
+++ dining/views.py
@@ -18,40 +18,36 @@ def test(request):
 def receipt(request, program_id):
     username = request.data.get("username")
     if not username:
-        return response.Response({"message": "نام کاربری فرستاده نشده است.", "name": "", "ok": False}, status=400)
+        return response.Response({"message": "نام کاربری فرستاده نشده است.", "name": "", "ok": False})
     profile = Profile.objects.filter(user__username=username).first()
     if not profile:
-        return response.Response({"message": "کاربر وارد شده وجود ندارد.", "name": "", "ok": False}, status=404)
+        return response.Response({"message": "کاربر وارد شده وجود ندارد.", "name": "", "ok": False})
     program = Program.objects.filter(id=program_id).first()
     if not program:
-        return response.Response({"message": "برنامه درخواست شده وجود ندارد.", "name": profile.__str__(), "ok": False},
-                                 status=404)
+        return response.Response({"message": "برنامه درخواست شده وجود ندارد.", "name": profile.__str__(), "ok": False})
     meal = Meal.objects.filter(program=program, start_time__lte=datetime.now(), end_time__gte=datetime.now()).first()
     if not meal:
         return response.Response(
-            {"message": "وعده غذایی‌ای در این زمان وجود ندارد.", "name": profile.__str__(), "ok": False}, status=404)
+            {"message": "وعده غذایی‌ای در این زمان وجود ندارد.", "name": profile.__str__(), "ok": False})
     registration = Registration.objects.filter(profile=profile, program=program, status='certain').first()
     if not registration:
         return response.Response(
-            {"message": "شما در این برنامه شرکت نکرده اید.", "name": profile.__str__(), "ok": False}, status=403)
+            {"message": "شما در این برنامه شرکت نکرده اید.", "name": profile.__str__(), "ok": False})
     reception = FoodReception.objects.filter(meal=meal, profile=profile).first()
     if not reception:
         reception = FoodReception(meal=meal, profile=profile, status='receipt')
         reception.save()
-        return response.Response({"message": "دریافت غذا با موفقیت انجام شد", "name": profile.__str__(), "ok": True},
-                                 status=200)
+        return response.Response({"message": "دریافت غذا با موفقیت انجام شد", "name": profile.__str__(), "ok": True})
     if reception.status == "reserved":
         reception.status = "receipt"
         reception.save()
-        return response.Response({"message": "دریافت غذا با موفقیت انجام شد", "name": profile.__str__(), "ok": True},
-                                 status=200)
+        return response.Response({"message": "دریافت غذا با موفقیت انجام شد", "name": profile.__str__(), "ok": True})
     elif reception.status == "receipt":
-        return response.Response({"message": "قبلا غذا دریافت نموده اید.", "name": profile.__str__(), "ok": False},
-                                 status=403)
+        return response.Response({"message": "قبلا غذا دریافت نموده اید.", "name": profile.__str__(), "ok": False})
     elif reception.status == "cancel":
         return response.Response(
-            {"message": "رزرو غذای خود را لغو کرده بودید.", "name": profile.__str__(), "ok": False}, status=403)
-    return response.Response({"message": "خطای ناشناس", "name": profile.__str__(), "ok": False}, status=406)
+            {"message": "رزرو غذای خود را لغو کرده بودید.", "name": profile.__str__(), "ok": False})
+    return response.Response({"message": "خطای ناشناس", "name": profile.__str__(), "ok": False})
 
 
 @decorators.api_view(['GET'])
@@ -70,12 +66,12 @@ def status(request, program_id):
     }
     program = Program.objects.filter(id=program_id).first()
     if not program:
-        return response.Response(resp, status=404)
+        return response.Response(resp)
     resp['program'] = program.title
     meal = Meal.objects.filter(program=program, start_time__lte=datetime.now(), end_time__gte=datetime.now()).first()
     if not meal:
         resp['message'] = "وعده غذایی‌ای در این زمان وجود ندارد."
-        return response.Response(resp, status=404)
+        return response.Response(resp)
     resp['ok'] = True
     resp['message'] = "اطلاعات ارسال شد."
     resp['meal']['title'] = meal.title
@@ -85,4 +81,4 @@ def status(request, program_id):
     resp['meal']['total'] = total
     resp['meal']['receipt_count'] = len(FoodReception.objects.filter(meal=meal, status='receipt'))
 
-    return response.Response(resp, status=200)
+    return response.Response(resp)
